0b696659bbd747eb4cb63ae354595607c79cd6b2
Ai engineer SisayThaddeus <160520835+Sisyetad@users.noreply.github.com>
2025-01-08 20:42:55 +0300

    profile page

--- frontend/src/scripts/js/login.js
+++ frontend/src/scripts/js/login.js
@@ -7,19 +7,16 @@ if (!loginForm || !signupForm || !toggleButton) {
     console.error("One or more DOM elements are missing. Please check your HTML structure.");
 }
 else {
-    // Function to handle the signup form display
     function signup() {
         loginForm.style.left = "-400px";
         signupForm.style.left = "50px";
         toggleButton.style.left = "110px";
     }
-    // Function to handle the login form display
     function login() {
         loginForm.style.left = "50px";
         signupForm.style.left = "450px";
         toggleButton.style.left = "0px";
     }
-    // Attach functions to the global scope for use in the HTML
     window.signup = signup;
     window.login = login;
 }
